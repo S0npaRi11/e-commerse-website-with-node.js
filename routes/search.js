74e4b1af0207588bf4e554844d3839ae6dfd79b8
parag mahale <mario2020@pop-os.localdomain>
2020-07-31 17:10:49 +0530

    Search and other UI changes

--- routes/search.js
+++ routes/search.js
@@ -4,17 +4,18 @@ const inventory = require('../models/Inventory');
 const router = express.Router();
 
 router.get('/search', (req,res) => {
-    res.render('../views/search.ejs',{searchResults: ['a']})
+    res.render('../views/search.ejs',{searchResults: []})
 })
 
 router.post('/search', (req,res) => {
     // console.log(req.query.search); 
-    const required = req.body.search.trim();
-    console.log(required);
+    // console.log('here')
+    const required = req.body.search.toLowerCase();
+    // console.log(required);
     inventory.find({$or:[{class: required}, {brand:required}, {name: required}]}, (err, products) => {
         if(err) console.log(err);
         else{
-            // console.log(product);
+            // console.log(products);
             res.render('../views/search.ejs',{searchResults: products})
         }
 
